5bc0f4a7549d2e0c3f61b03e4491d989e6f2f5fe
Sullivan Devroede <sullivan.devroede@gmail.com>
2018-01-18 16:52:00 +0100

    Create base of the program

---
+++ Project Hierarchy Generator/Project Hierarchy Generator/Project Hierarchy Generator/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace ProjectHierarchyGenerator
+{
+    class Program
+    {
+
+        private string folderPath = string.Empty;
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.AskFolderPath();
+        }
+
+        void AskFolderPath()
+        {
+            Console.Clear();
+            Console.WriteLine("Write your Folder Path and press Enter");
+            string folderPath = Console.ReadLine();
+            if (Directory.Exists(folderPath))
+            {
+                CheckFolderPath();
+            }
+            else
+            {
+                Console.WriteLine("The Folder Path lead to nothing please create the folder before");
+                AskFolderPath();
+            }
+        }
+
+        void CheckFolderPath()
+        {
+            Console.WriteLine("Your Folder path is : " + folderPath);
+            Console.WriteLine("Is it the good folder path Yes or No :");
+            string input = Console.ReadLine();
+            if (input == "Yes" || input == "yes")
+            {
+                CheckFolderContent();
+            }
+            else if (input == "No" || input == "no")
+            {
+                AskFolderPath();
+            }
+            else
+            {
+                Console.WriteLine("Type Help for more commands");
+                ShowHelpMenu();
+            }
+        }
+
+        void CheckFolderContent()
+        {
+            string[] fileList = Directory.GetFiles(folderPath);
+            if (fileList.Length != 0)
+            {
+                Console.WriteLine("The folder must be empty - (Type Clear if you want to clear it)");
+                CommandBuffer();
+            }
+
+        }
+
+        void CommandBuffer()
+        {
+            string newInput = Console.ReadLine();
+            switch (newInput)
+            {
+                //HELP
+                case "Help":
+                    ShowHelpMenu();
+                    break;
+                case "help":
+                    ShowHelpMenu();
+                    break;
+
+                //RESTART
+                case "Restart":
+                    AskFolderPath();
+                    break;
+                case "restart":
+                    AskFolderPath();
+                    break;
+
+                //QUIT
+                case "Quit":
+                    System.Environment.Exit(0);
+                    break;
+                case "quit":
+                    System.Environment.Exit(0);
+                    break;
+            }
+        }
+
+        void ShowHelpMenu()
+        {
+            Console.WriteLine("Help - Give the commands list");
+            Console.WriteLine("Restart - Restart the application.");
+            Console.WriteLine("Quit - Close the application.");
+            string input = Console.ReadLine();
+            CommandBuffer();
+        }
+    }
+}
--- Project Hierarchy Generator/Project Hierarchy Generator/Project Hierarchy Generator/Properties/AssemblyInfo.cs
+++ Project Hierarchy Generator/Project Hierarchy Generator/Project Hierarchy Generator/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("Project Hierarchy Generator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("Project Hierarchy Generator")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0086e086-360c-464c-aa02-911123b9a312")]
+[assembly: Guid("92d61a98-2ce8-463f-b94d-e306c6033097")]
 
 // Version information for an assembly consists of the following four values:
 //
